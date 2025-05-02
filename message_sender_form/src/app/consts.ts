dda8410fac7e4c15d92350b3a9d4ac332493c041
Rhavy Frota <rhavyf1411@gmail.com>
2025-05-02 17:54:00 -0300

    consts updated and impoved audio

--- message_sender_form/src/app/consts.ts
+++ message_sender_form/src/app/consts.ts
@@ -1,4 +1,3 @@
 
-export const API_PATH : string= "http://127.0.0.1:8000";
-
-
+export const API_PATH : string= "https://podcast-donate-simulator.onrender.com/";
+export const WS_PATH : string = "wss://podcast-donate-simulator.onrender.com//register/ex"
